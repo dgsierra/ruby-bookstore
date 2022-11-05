a8488d4bf74685a71898cd0079b3d8e8d88b7ef7
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-05 13:18:57 -0600

    Add Nameable class and linked to person

--- person.rb
+++ person.rb
@@ -1,8 +1,10 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @name = name
     @parent_permission = parent_permission
     @id = 'Unknown'
@@ -14,8 +16,16 @@ class Person
     @age >= 18
   end
 
+  def correct_name
+    @name
+  end
+
   def can_use_services?
     @parent_permission or of_age?
   end
   private :of_age?
 end
+
+perso = Person.new(20,'', parent_permission: true)
+
+p perso.correct_name
