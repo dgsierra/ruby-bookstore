07c53dcd6601e9bc31ebf4441f037e6951e7ccd7
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-04 22:17:46 -0600

    Fix given int to age of person

--- person.rb
+++ person.rb
@@ -2,19 +2,24 @@ class Person
   attr_accessor :name, :age
   attr_reader :id, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name.chomp
     @parent_permission = parent_permission
     @id = 'Unknown'
-    @age = 19
+    @age = age
   end
 
-  def of_age? # Name changed of is_of_age? to avoid Rubocop error
+  # Name changed of is_of_age? to avoid Rubocop error
+  def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    @parent_permission or is_of_age?
+    @parent_permission or of_age?
   end
   private :of_age?
 end
+
+myperson = Person.new(19, 'Daniel', parent_permission: false)
+
+p myperson.parent_permission
