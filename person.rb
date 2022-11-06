1ae6fcb3461fcb53aebeebb42057f98c3a9f35b0
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-05 19:52:53 -0600

    Remove returns (for testing) in rental

--- person.rb
+++ person.rb
@@ -24,7 +24,7 @@ class Person < Nameable
 
   def new_rental(date, book)
     p self
-    Rental.new(date ,self , book)
+    Rental.new(date, self, book)
   end
 
   def can_use_services?
@@ -34,6 +34,5 @@ class Person < Nameable
 end
 
 test2 = Person.new(1, 20, 'test')
-
 
 p test2.new_rental('2021-01-01', 'test')
