ca3e32a56a8ac34a0eb5b90859d8f7602818ef09
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-05 20:11:30 -0600

    Add console message

--- main.rb
+++ main.rb
@@ -20,3 +20,15 @@ class App
     end
     puts 'Thank you for using this app!'
   end
+
+  def display_options
+    puts
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
