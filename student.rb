8b17de70df003370133f797433ea9b966f57c718
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-05 21:16:02 -0600

    Add initializer parameters

--- student.rb
+++ student.rb
@@ -2,9 +2,9 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize
+  def initialize(id, age, classroom, name = 'Unknown Student', parent_permission: true)
     super(age, name)
-    @classroom = 'Unknown'
+    @classroom = classroom
   end
 
   def play_hooky
